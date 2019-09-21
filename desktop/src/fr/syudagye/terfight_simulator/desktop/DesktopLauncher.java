93e48b4940152da346b634674b41be145c7c1d5c
Syudagye <maximepico@free.fr>
2019-09-21 23:32:55 +0200

    first commit

---
+++ desktop/src/fr/syudagye/terfight_simulator/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.syudagye.terfight_simulator.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.syudagye.terfight_simulator.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
